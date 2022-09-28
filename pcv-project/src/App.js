5eac87867b9c62c6223619bac196616558576929
Ghers Fisman <ghersf91@gmail.com>
2022-09-28 21:02:29 +0200

    Create React App, clean and name app

---
+++ pcv-project/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Página Web Placivel!
+        </h1>
+        <h4>
+          By: Ghers Fisman
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
